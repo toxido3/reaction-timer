e96d3542f0693f655245d8a0c9e8616ecf4b42ca
Toxido3 <ilker.ihsan.eksioglu@gmail.com>
2023-11-21 00:02:30 +0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Welcome to timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @endGame="endGame"></Block>
+  <Results v-if="showResults" :score="score"></Results>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block,
+    Results
+  },
+  data(){
+    return{
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random()*5000;
+      this.isPlaying = true;
+      this.showResults = false;
+      console.log(this.delay);
+    },
+    endGame(reactionTime){
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    }
   }
 }
 </script>
@@ -20,7 +45,24 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <p>Reaction time: {{score}} ms</p>
+        <p class="rank">{{rank}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Results',
+        props: ['score'],
+        data(){
+            return{
+                rank: null
+            }
+        },
+        mounted(){
+            if(this.score < 250){
+                this.rank = "You are fast cowboy :)";
+            }
+            else if(this.score < 400){
+                this.rank = "You are fast"
+            }else{
+                this.rank = "Snail pace..."
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.rank{
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
+</style>
